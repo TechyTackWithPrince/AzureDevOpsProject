c8dfdaa960bd0cf643892e6c1085c130f22aa126
TechyTackWithPrince <princeninawe1996@gmail.com>
2023-10-26 15:09:02 +0530

    Added Virtual Macine in TF

--- TerraformFiles/virtual_network.tf
+++ TerraformFiles/virtual_network.tf
@@ -9,20 +9,34 @@ resource "azurerm_virtual_network" "network" {
   location            = azurerm_resource_group.res_group.location
   resource_group_name = azurerm_resource_group.res_group.name
   address_space       = ["192.168.0.0/16"]
-#   dns_servers         = ["10.0.0.4", "10.0.0.5"]
+  #   dns_servers         = ["10.0.0.4", "10.0.0.5"]
 
-  subnet {
-    name           = "subnet1"
-    address_prefix = "192.168.1.0/24"
-  }
+  #   subnet {
+  #     name           = "subnet1"
+  #     address_prefix = "192.168.1.0/24"
+  #   }
 
-  subnet {
-    name           = "subnet2"
-    address_prefix = "192.168.2.0/24"
-    security_group = azurerm_network_security_group.network-sg.id
-  }
+  #   subnet {
+  #     name           = "subnet2"
+  #     address_prefix = "192.168.2.0/24"
+  #     security_group = azurerm_network_security_group.network-sg.id
+  #   }
 
   tags = {
     creator = "prince"
   }
+}
+
+resource "azurerm_subnet" "sub1" {
+  name                 = "subnet1"
+  resource_group_name  = azurerm_resource_group.res_group.name
+  virtual_network_name = azurerm_virtual_network.network.name
+  address_prefixes     = ["192.168.1.0/24"]
+}
+
+resource "azurerm_subnet" "sub2" {
+  name                 = "subnet2"
+  resource_group_name  = azurerm_resource_group.res_group.name
+  virtual_network_name = azurerm_virtual_network.network.name
+  address_prefixes     = ["192.168.2.0/24"]
 }
---
+++ TerraformFiles/virtualmachine.tf
@@ -0,0 +1,57 @@
+resource "azurerm_public_ip" "ip1" {
+  name                = "publicip1"
+  location            = azurerm_resource_group.res_group.location
+  resource_group_name = azurerm_resource_group.res_group.name
+  allocation_method   = "Dynamic"  # You can also use "Static" for a static IP
+
+  tags = {
+    creator = "prince"
+  }
+}
+
+resource "azurerm_network_interface" "nic1" {
+  name                = "uat-nic"
+  location            = azurerm_resource_group.res_group.location
+  resource_group_name = azurerm_resource_group.res_group.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.sub1.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id           = azurerm_public_ip.ip1.id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "uatvm" {
+  name                = "uat-vm"
+  resource_group_name = azurerm_resource_group.res_group.name
+  location            = azurerm_resource_group.res_group.location
+  size                = "Standard_B1s"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.nic1.id,
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts-gen2"
+    version   = "latest"
+  }
+    custom_data = base64encode(<<EOF
+#!/bin/bash
+apt-get update
+apt-get -y install python
+EOF
+)
+}
