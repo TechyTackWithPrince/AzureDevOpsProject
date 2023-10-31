213f6f1e3adef45574a8b385a53bea949902a5a9
TechyTackWithPrince <princeninawe1996@gmail.com>
2023-10-31 20:31:18 +0530

    Updated Readme.md

--- TerraformFiles/main.tf
+++ TerraformFiles/main.tf
@@ -4,12 +4,6 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "3.77.0"
     }
-  # }
-  # backend "azurerm" {
-  #   resource_group_name  = "first_project"
-  #   storage_account_name = "princestorage99999"
-  #   container_name       = "tfstate"
-  #   key                  = "terraform.tfstate"
   }
 }
 
